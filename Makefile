e418af484c012d25a9618c79319391bcb8cb1715
tangkhanhchuong <chuongtangkhanh2104@gmail.com>
2021-11-19 08:50:20 +0700

    connect nodejs to mongodb container

--- Makefile
+++ Makefile
@@ -2,14 +2,14 @@
 setup:
 	@echo "========== Start installing dependencies =========="
 
-	sudo docker build -t "express_server" .
+	docker build -t "express_server" .
 	
 	@echo "========== Environment is ready! App is running! =========="
 
 start:
 	@echo "========== Start running app =========="
 
-	sudo docker-compose up
+	docker-compose up
 
 	
 cleanup:
--- src/services/database.js
+++ src/services/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_DATABASE } = process.env
 // const uri = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.upzro.mongodb.net/${MONGO_DATABASE}?retryWrites=true&w=majority`
 
-const uri = `mongodb://localhost:27017/udemy`
+const uri = `mongodb://mongo:27017`
 
 const mongooseOptions = {
   useNewUrlParser: true,
